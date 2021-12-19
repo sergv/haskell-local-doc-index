c9470985bff6671371dacddad7c9d0b17baa1b8e
Sergey Vinokurov <serg.foo@gmail.com>
2021-12-19 18:24:14 +0000

    Collect list of files when collecting tags

--- update-tags.sh
+++ update-tags.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 time fast-tags -o tags -v -R . --nomerge
+
+find all-packages \( -name '*.hs' -o -name '*.lhs' -o -name '*.x' -o -name '*.y' -o -name '*.lx' -o -name '*.ly' -o -name '*.chs' -o -name '*.hsc' -o -name '*.h' -o -name '*.c' -o -name '*.hpp' -o -name '*.cpp' -o -name '*.hxx' -o -name '*.cxx' -o -name '*.hh' -o -name '*.cc' \) -print >files.txt

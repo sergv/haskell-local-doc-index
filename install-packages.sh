4fafbd450a41f796ef48f086fa75da62a5cd258d
Sergey Vinokurov <serg.foo@gmail.com>
2022-01-02 00:04:00 +0000

    Correctly invoke haddock so that even unpatched version will produce package names

--- install-packages.sh
+++ install-packages.sh
@@ -23,9 +23,9 @@ cabal="./cabal-3.7-no-asserts"
 
 # haddock=/home/sergey/projects/haskell/projects/thirdparty/haddock/dist-newstyle/build/x86_64-linux/ghc-8.6.5/haddock-2.22.0/x/haddock/build/haddock/haddock
 # haddock=/home/sergey/projects/haskell/projects/thirdparty/haddock/dist-newstyle/build/x86_64-linux/ghc-8.8.2/haddock-2.23.0/x/haddock/build/haddock/haddock
-haddock=/home/sergey/projects/haskell/projects/thirdparty/haddock/dist-newstyle/build/x86_64-linux/ghc-8.10.7/haddock-2.24.2/x/haddock/build/haddock/haddock
+# haddock=/home/sergey/projects/haskell/projects/thirdparty/haddock/dist-newstyle/build/x86_64-linux/ghc-8.10.7/haddock-2.24.2/x/haddock/build/haddock/haddock
 
-# haddock=haddock
+haddock=haddock
 # haddock=/home/sergey/projects/haskell/projects/thirdparty/haddock/dist/build/haddock/haddock
 #haddock=/home/sergey/projects/haskell/projects/thirdparty/haddock/dist-newstyle/build/x86_64-linux/ghc-8.4.3/haddock-2.20.0/x/haddock/build/haddock/haddock
 # haddock=/home/sergey/projects/haskell/projects/thirdparty/haddock/.stack-work/install/x86_64-linux-tinfo6/lts-12.5/8.4.3/bin/haddock
@@ -36,8 +36,8 @@ haddock_theme_dir="/home/sergey/projects/haskell/projects/thirdparty/haddock/had
 
 export haddock_api_datadir=/home/sergey/projects/haskell/projects/thirdparty/haddock/haddock-api/resources
 
-pkg_db_dir="$root/local/pkg-db.d"
-store_pkg_db_dir="local-store/ghc-${ghc_version}/package.db/"
+# pkg_db_dir="$root/local/pkg-db.d"
+store_pkg_db_dir="$root/local-store/ghc-${ghc_version}/package.db/"
 docs_dir="$root/docs"
 
 html_dir="html"
@@ -302,13 +302,19 @@ if [[ "$action" = "generate-haddock-docs" || "$action" = "all" ]]; then
 
         echo "Running haddock"
 
-            # "--css=$haddock_css" \
+        global_pkg_db="$(ghc-pkg list --global | head -n1)"
+
+        # Passing explicit package dbs makes even unpatched haddock generate
+        # package names.
+        # cf https://github.com/commercialhaskell/stack/pull/3226
         "$haddock" \
             --verbosity=3 \
             --pretty-html \
             --gen-contents \
             --gen-index \
             --odir="$docs_dir" \
+            --optghc=-package-db="$global_pkg_db" \
+            --optghc=-package-db="$store_pkg_db_dir" \
             --title="Standalone Haskell Documentation" \
             --hyperlinked-source \
             $haddock_args

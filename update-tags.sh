e075fdc48297179d3f8e21bfc78447b5cd46079f
Sergey Vinokurov <serg.foo@gmail.com>
2022-09-22 23:37:29 +0100

    Fix shebang

--- update-tags.sh
+++ update-tags.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#! /usr/bin/env bash
 
 find all-packages \( -path "all-packages/ghc-exactprint-*/tests/*" -o -path "all-packages/semigroups-*/src-ghc7/*" -o -path "all-packages/transformers-*/legacy/*" \) -prune -o \( -name '*.hs' -o -name '*.lhs' -o -name '*.chs' -o -name '*.hsc' -o -name '*.sig' -o -name '*.lsig' \) -print0 | fast-tags -0 -o tags -v --nomerge -
 

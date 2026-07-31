6c21fce87e9e71e235409568035d7d0390dfe526
Sergey Vinokurov <serg.foo@gmail.com>
2026-08-01 00:12:01 +0100

    hiedb is not needed for now

--- flake.nix
+++ flake.nix
@@ -152,7 +152,7 @@
             ghc
             packages.cabal
 
-            hiedb
+            # hiedb
           ] ++ map (x: if builtins.hasAttr "dev" x then x.dev else x) nativeDeps;
 
           LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath nativeDeps;
